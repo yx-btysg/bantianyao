6b4178b89a43fd74d65878db2146fcc02581cce7
bo gao <gaobo.astar@google.com>
2025-11-11 08:21:00 +0800

    new bedok store

--- src/components/StoreDistribution.tsx
+++ src/components/StoreDistribution.tsx
@@ -7,7 +7,7 @@ const StoreDistribution: React.FC = () => {
                 <div className="px-6">
                     <h2 className="text-3xl font-bold text-gray-800 mb-4">Find a Store Near You</h2>
                     <p className="text-gray-600 mb-12 max-w-6xl mx-auto">
-                        With over 1000 locations, a delicious Bantianyin experience is always within reach. Explore our stores across China.
+                        With over 1000 locations, a delicious Bantianyao experience is always within reach. Explore our stores across Singapore.
                     </p>
                 </div>
                 <div className="mt-12 flex justify-center">
@@ -30,12 +30,14 @@ const StoreDistribution: React.FC = () => {
                         <div className="bg-white p-6 rounded-lg shadow-md">
                             <h3 className="text-xl font-bold text-cyan-500 mb-2">AMK Store</h3>
                             <p className="text-gray-600">61 Ang Mo Kio Avenue 8, #01-07/08/09/10 Jubilee Square, Singapore 569814
-                            <br /><span className="font-bold">Tel: </span> +65 8850 2844
+                            <br /><span className="font-bold">Tel: </span> +65 9687 5229
                             </p>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow-md">
-                            <h3 className="text-xl font-bold text-cyan-500 mb-2">Flagship3</h3>
-                            <p className="text-gray-600">Coming Soon</p>
+                            <h3 className="text-xl font-bold text-cyan-500 mb-2">Bedok Store</h3>
+                            <p className="text-gray-600">215 Bedok North Street 1, #01-83, Singapore 460215
+                            <br /><span className="font-bold">Tel: </span> +65 9687 5229
+                            </p>
                         </div>
                     </div>
                 </div>
